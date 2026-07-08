e4552b9cd9f9c085f80cd63ab6ddc32b7dfa2b80
Emily_Pereira <Emily07ribeiro@gmail.com>
2026-07-08 18:05:09 -0300

    feat(leitura&condicional): Adição de variaveis teste e scanner pra leitura de terminal

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,22 @@ public class Main {
         boolean incluidoNoPlano = true;
         double notaDoFilme =8.1;
         String nomeFilme ="Filme";
+
+        double media = (9.8+ 6.3 +8.0)/3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """ 
+                Filme Top Gun 
+                Filme de aventura com galã dos anos 80 
+                Muito bom!
+                Ano de lançamento """+ anoDeLancamento;
+
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media/2);
+        System.out.println(classificacao);
+
+
     }
 
 
